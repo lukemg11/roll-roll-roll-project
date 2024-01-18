3d08797ef5e7ef4e1fa517cc7b440ac3e7f2625b
lukemg11 <145138956+lukemg11@users.noreply.github.com>
2024-01-18 10:54:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
-var dice = 1;
-var sumOfRoll = 0;
-const roll1 = document.getElementById("roll1");
-roll1.addEventListener("click", rollDice())
-function rollDice(){
-    sumOfRoll=Math.trunc((Math.random()*6 + 1));
-    console.log("sumOfRoll = " + sumOfRoll);
-};
+var dice = 0;
+var numRolls = 0;
+let mean = 0;
+let median = 0;
+let mode = 0;
+let totalSum = 0;
+let arrOfRolls = [];
+let ones = 0;
+let twos = 0;
+let form = document.getElementById('questions');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    numRolls = form.elements['rolls'].value;
+    dice = form.elements['dicenum'].value;
+})
+let roll = document.getElementById("roll1");
+roll.addEventListener("click", rollDice)
+
+// function rollDice(){
+//     sumOfRoll=Math.trunc((Math.random()*6 + 1));
+//     console.log("sumOfRoll = " + sumOfRoll);
+// };
 // roll1.addEventListener("click", rollDice());
-rollDice();
+function rollDice(){
+    for (let i = 0; i < numRolls; i++) {
+        for (let i = 0; i < dice; i++) {
+            let num = Math.floor(((Math.random()*6)+1))
+            arrOfRolls.push(num);
+            totalSum += num;
+        }
+      }
+      console.log(totalSum);
+      findMean();
+      findMedian();
+      console.log(arrOfRolls);
+};
+function findMean(){
+    mean = totalSum/numRolls;
+    document.getElementById("meanval").innerHTML = mean;
+}
+function findMedian(){
+    arrOfRolls.sort()
+    median = arrOfRolls[arrOfRolls.length/2];
+    document.getElementById("medval").innerHTML = median;
+}
+function findMode(){
+    mode = 3;
+    document.getElementById("modeval").innerHTML = mode;
+}
+
+
